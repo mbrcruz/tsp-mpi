c3b05d5a9246f272518d69bafebe906a4a4cb2d8
EC2 Default User <ec2-user@ip-172-16-152-82.ec2.internal>
2023-09-20 16:09:06 +0000

    time log

--- src/tsp-mpi.c
+++ src/tsp-mpi.c
@@ -464,9 +464,9 @@ int main(int argc, char *argv[]) {
     selection(pops, new_pops, pop_size, coords, n_cities);
 
     // Uncomment for periodic updates
-     if ((i % 100) == 0) {
+     if ((i % 1000) == 0) {
        printf("Process %d generation %zu\t", id, i + 1);
-       FitnessStatus(pops, coords, pop_size, n_cities);
+       //FitnessStatus(pops, coords, pop_size, n_cities);
      }
   }
   size_t my_best_path[1];
